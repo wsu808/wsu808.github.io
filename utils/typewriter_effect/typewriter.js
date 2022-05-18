313c3c596c03740d72ba2b32c9aaaa8ce456d47f
wasu <11012001db@gmail.com>
2022-05-18 12:07:59 +0200

    add instruction to script as comment

--- utils/typewriter_effect/typewriter.js
+++ utils/typewriter_effect/typewriter.js
@@ -1,10 +1,12 @@
-/* Typerwriter Effect
-* in HTML: <div id="console" onclick="typeWriter('console','sad|asdw')">coś</div>
-* use | character instead of line breaks
-* typeWriter(id,txt)
-* or
-* typeWriter(id,txt,speed)
-* @author wsu808.github.io
+// Typerwriter Effect
+// @author wsu808.github.io
+/* Paste this in your HTML; last parameter (speed/timeout) is optional
+* <script src="https://raw.githubusercontent.com/wsu808/wsu808.github.io/main/utils/typewriter_effect/typewriter.js">
+* <script>
+*       window.addEventListener("DOMContentLoaded", () => 
+*           typeWriter("element_id", "some text | some text in new line",100)
+*       );
+*  </script>
 */
 
 //LOAD CSS
@@ -18,16 +20,11 @@ document.getElementsByTagName('HEAD')[0].appendChild(link);
 
 function typeWriter(id, txt, speed = 200) {
     let elem = document.getElementById(id);
-    //elem.classList.add("typewriter");
-    //elem.after(document.createElement("br")); //add line break AFTER typewriter elment
     elem.innerHTML = "<span class='typewriter-content'></span><span class='typewriter-blinker'></span>";
-
-
-    var i = 0;
-    type(id, txt, speed, i);
+    type(id, txt, speed);
 }
 
-function type(id, txt, speed, i) {
+function type(id, txt, speed, i=0) {
     if (i < txt.length) {
         let elem = document.getElementById(id).getElementsByClassName("typewriter-content")[0];
         if (txt.charAt(i)=="|") { //print line break
