e65f52057bb3164f36db2f454fde1aa7e60325c0
wasu <11012001db@gmail.com>
2022-05-18 12:10:03 +0200

    Delete unused script
    New wersion in utils/typewriter_effect

--- assets/js/typewriter.js
+++
@@ -1,9 +0,0 @@
-var i = 0;
-
-function typeWriter(txt,speed,id) {
-  if (i < txt.length) {
-    document.getElementById(id).innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
